470a8241b059a4241d6a3b81739965afc65a5011
jjw <js48jjw@daum.net>
2025-12-10 08:55:17 +0900

    dbsense remove

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,26 +38,6 @@ export default function RootLayout({
         aria-label="Block Stacking Game App"
       >
         {children}
-        {/* Left Promo Banner */}
-        <a
-          className="promo-banner left"
-          href="https://pmioham9d3.sens.kr"
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label="왼쪽 홍보 배너"
-        >
-          <img src="/dbsense-banner-left.png" alt="왼쪽 홍보 배너" />
-        </a>
-        {/* Right Promo Banner */}
-        <a
-          className="promo-banner right"
-          href="https://ig8rt9xz3i.sens.kr"
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label="오른쪽 홍보 배너"
-        >
-          <img src="/dbsense-banner-right.png" alt="오른쪽 홍보 배너" />
-        </a>
         <ServiceWorkerRegister />
       </body>
     </html>
